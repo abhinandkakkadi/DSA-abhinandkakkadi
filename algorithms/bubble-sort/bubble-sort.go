3c31396ebd42264f050d395c199963f08e863a52
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-03-29 11:27:26 +0530

    Added some more bubble sort operaions

--- algorithms/bubble-sort/bubble-sort.go
+++ algorithms/bubble-sort/bubble-sort.go
@@ -4,10 +4,20 @@ import "fmt"
 
 func main() {
 
-	arr := []int {1,2,3,4,5}
+	arr := []int {3,1,5,4,2}
 
 	bubbleSort(arr)
 	fmt.Println(arr)
+
+	// bubbleSort Reverse order
+	arr2 := []int{1,4,5,1,2}
+	bubbleReverse(arr2)
+	fmt.Println(arr2)
+
+	// sort only a portion of the array specified from last
+	bubblePortion(arr2,2)
+	fmt.Println(arr2)
+
 }
 
 func bubbleSort(arr []int) {
@@ -21,9 +31,53 @@ func bubbleSort(arr []int) {
 				swap = true
 				arr[j],arr[j-1] = arr[j-1],arr[j]
 			}
+
 		}
 		if !swap {   // If swap is false that the array is sorted
 			break
 		} 
+	}
+}
+
+// Bubble sort reverse
+
+func bubbleReverse(arr2 []int) {
+
+	for i:=0; i < len(arr2); i++ {
+
+		swap := false
+
+		for j:=1; j< len(arr2) - i; j++ {
+			
+			if arr2[j] > arr2[j-1] {
+				swap = true
+				arr2[j], arr2[j-1] = arr2[j-1], arr2[j]
+			}
+		}
+
+		if !swap {
+			break
+		}
+
+	}
+}
+
+// sort only a portion of the array specified from last
+func bubblePortion(arr2 []int,portion int) {
+
+	for i:=0; i < portion; i++ {
+		swap := false
+		for j:=1; j < len(arr2) - i; j++ {
+			
+			if arr2[j] < arr2[j-1] {
+				swap = true
+				arr2[j], arr2[j-1] = arr2[j-1],arr2[j]
+			}
+
+		}
+
+		if !swap {
+			break
+		}
 	}
 }
